15301a8f9e571147361c2ee12822b90c16df911f
LarsG <lars.gunnar@jobloop.no>
2023-06-24 15:32:48 +0000

    setup basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <header className='Navbar'>
+        <div>
+          <h1>Layout Component</h1>
+          <h2>Example of a Layout Component with a navigation sidebar</h2>
+        </div>
+      </header>
+
+      <main className='Main'>
+
+      </main>
+
+      <footer className='Footer'>
+        <h2>
+          &copy; Lars Gunnar
+        </h2>
+      </footer>
+    </div>
   )
 }
 
