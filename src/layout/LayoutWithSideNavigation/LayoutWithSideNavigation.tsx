32c0f276ef277ef331a55b5e2fcc0588d9ee3638
LarsG <lars.gunnar@jobloop.no>
2023-06-24 17:41:25 +0000

    add nav links flexibility

--- src/layout/LayoutWithSideNavigation/LayoutWithSideNavigation.tsx
+++ src/layout/LayoutWithSideNavigation/LayoutWithSideNavigation.tsx
@@ -1,5 +1,34 @@
-import React, { HTMLAttributes } from "react";
+import React, { HTMLAttributes, createContext, useContext } from "react";
 import style from "./style.module.css";
+
+const layoutContext = createContext<ILayoutContext | undefined>(undefined);
+
+interface ILayoutContext {
+  headers: string[];
+}
+
+interface LayoutContextProps {
+  value: ILayoutContext;
+  children: React.ReactNode;
+}
+
+const LayoutContext = ({ value, children }: LayoutContextProps) => {
+  return (
+    <layoutContext.Provider value={value}>{children}</layoutContext.Provider>
+  );
+};
+
+const useLayoutContext = () => {
+  const context = useContext(layoutContext);
+
+  if (!context) {
+    throw new Error(
+      "This component must be used within the Layout.Root component"
+    );
+  }
+
+  return context;
+};
 
 interface RootProps {
   children: [
@@ -11,20 +40,35 @@ interface RootProps {
 const Root = ({ children }: RootProps) => {
   const [navigation, content] = children;
 
+  const headers = ["Hello", "World", "Fooo", "Baaard"];
+
   return (
-    <section className={style["container"]}>
-      <div className={style["navigation"]}>{navigation}</div>
-      <div className={style["content"]}>{content}</div>
-    </section>
+    <LayoutContext value={{ headers }}>
+      <section className={style["container"]}>
+        <div className={style["navigation"]}>{navigation}</div>
+        <div className={style["content"]}>{content}</div>
+      </section>
+    </LayoutContext>
   );
 };
 
-interface NavigationProps {}
+interface NavigationProps {
+  linksAs?: (title: string) => JSX.Element;
+}
 
-const Navigation = (props: NavigationProps) => {
+const Navigation = ({ linksAs }: NavigationProps) => {
+  const { headers } = useLayoutContext();
+
   return (
-    <nav>
+    <nav className="">
       <h2>Navigation</h2>
+      <ul>
+        {headers.map((heading) => (
+          <li key={heading}>
+            <a href={`#${heading}`}>{linksAs ? linksAs(heading) : heading}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
